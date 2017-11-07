319a1511af6eec3d88f755f466cd6e8f52589ea2
ArkejGit <robert.krawczyk92@gmail.com>
2017-11-07 20:25:41 +0100

    Improve buttons view, position and logic

--- Timer.js
+++ Timer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Dimensions} from 'react-native';
 import {observer, inject} from 'mobx-react/native'
 
 
@@ -7,8 +7,14 @@ import {observer, inject} from 'mobx-react/native'
 
 @observer
 class TimerButton extends React.Component {
+
+  static navigationOptions = {
+    header: null
+  }
+
   render() {
     const { navigate } = this.props.navigation;
+    const winWidth = Dimensions.get('window').width;
 
     return (
       <View style={styles.container}>
@@ -26,6 +32,7 @@ class TimerButton extends React.Component {
           </View>
         </TouchableOpacity>
 
+        <View style={[styles.buttons, {width: winWidth}]}>
         { !this.props.store.gameInProgress && 
           <TouchableOpacity style={styles.startButton}
           onPress={() => {
@@ -39,19 +46,18 @@ class TimerButton extends React.Component {
         </TouchableOpacity>
         }
 
-        { this.props.store.gameInProgress && 
-          <TouchableOpacity style={styles.startButton}
-          onPress={() => {
-            this.props.store.clearGame();
-            navigate('Home');
-          }}
-          color='#72B01D'
-          >
-            <Text style={styles.startButtonText}>
-              BACK TO MENU
-            </Text>
+        <TouchableOpacity style={styles.startButton}
+        onPress={() => {
+          navigate('Home');
+          this.props.store.clearGame();
+        }}
+        color='#72B01D'
+        >
+          <Text style={styles.startButtonText}>
+            BACK TO MENU
+          </Text>
         </TouchableOpacity>
-        }
+        </View>
 
         <TouchableOpacity id="whiteContainer"
         style={styles.whiteContainer}
@@ -83,7 +89,8 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#232021'
+    backgroundColor: '#232021',
+    zIndex: 1,
   },
   whiteContainer: {
     flex: 1,
@@ -105,14 +112,19 @@ const styles = StyleSheet.create({
   black: {
     color: '#232021'
   },
+  buttons: {
+    position: 'absolute',
+    zIndex: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
   startButton: {
     backgroundColor: '#72B01D',
-    zIndex: 10,
     borderRadius: 2,
     elevation: 4,
     alignItems: 'center',
     justifyContent: 'center',
-    transform: [{ translateY: -25 }]
   },
   startButtonText: {
     color: '#F3EFF5',
